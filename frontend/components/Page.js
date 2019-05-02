e52503ab484ab58d89c7685c5fbab6f4447649bc
Evgeniy Morskoy <zhekamorskoi@gmail.com>
2019-05-02 19:59:10 +0300

    Nav styles

--- frontend/components/Page.js
+++ frontend/components/Page.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import Header from '../components/Header';
-import Meta from '../components/Meta';
 import styled, { ThemeProvider, injectGlobal } from 'styled-components';
+
+import Meta from '../components/Meta';
+import Header from '../components/Header';
 
 const theme = {
   red: '#ff0000',
@@ -21,6 +22,35 @@ const Inner = styled.div`
   max-width: ${props => props.theme.maxWidth};
   margin: 0 auto;
   padding: 2rem;
+`;
+
+injectGlobal`
+  @font-face {
+    font-family: 'radnika_next';
+    src: url('/static/radnikanext-medium-webfont.woff2') format('woff2');
+    font-weight: normal;
+    font-style: normal;
+  }
+  html {
+    box-sizing: border-box;
+    font-size: 10px;
+  }
+  *,
+  *:before,
+  *:after {
+    box-sizing: inherit;
+  }
+  body {
+    padding: 0;
+    margin: 0;
+    font-size: 1.5rem;
+    line-height: 2;
+    font-family: 'radnika_next';
+  }
+  a{
+    text-decoration: none;
+    color: ${theme.black};
+  }
 `;
 
 class Page extends Component {
